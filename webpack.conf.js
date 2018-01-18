a2da06aef08b863f33a3716246bce53971fc98d9
John Nolette <john@neetgroup.net>
2018-01-17 22:09:44 -0500

    Added google fonts plugin, sass and style loaders

--- webpack.conf.js
+++ webpack.conf.js
@@ -4,6 +4,9 @@ function resolve(dir) {
   return path.join(__dirname, '.', dir)
 }
 
+const GoogleFontsPlugin = require('google-fonts-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
   entry: './src/App.jsx',
   output: {
@@ -24,9 +27,36 @@ module.exports = {
             plugins: ['add-module-exports']
           }
         }
-      }
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader'
+        })
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      { test: /\.html$/, loader: 'raw-loader'},
+      { test: /\.(png|jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'url-loader' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
     ]
   },
+  plugins: [
+    new GoogleFontsPlugin({
+			fonts: [
+				{ family: 'Julius Sans One' },
+        { family: 'Zilla Slab Highlight' },
+        { family: 'Barlow Condensed' }
+			]
+		}),
+    new ExtractTextPlugin('dist.css')
+  ],
   externals : {
     document: 'document'
   }
