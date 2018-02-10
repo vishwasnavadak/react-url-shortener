5d9fb08fa017ea0737d69f80dfebd90ce86f23a9
John Nolette <john@neetgroup.net>
2018-02-10 14:01:32 -0500

    Updated routes dir

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Home from '@/router/routes/Home.jsx'
-import Shortened from '@/router/routes/Shortened.jsx'
-import About from '@/router/routes/About.jsx'
-import Signup from '@/router/routes/Signup.jsx'
-import Dashboard from '@/router/routes/Dashboard.jsx'
+import Home from '@/router/views/Home.jsx'
+import Shortened from '@/router/views/Shortened.jsx'
+import About from '@/router/views/About.jsx'
+import Signup from '@/router/views/Signup.jsx'
+import Dashboard from '@/router/views/Dashboard.jsx'
 
 
 export default class Router extends Component {
