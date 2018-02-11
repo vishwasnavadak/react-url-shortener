10a6cec91438a49f39b9d2791b37a734644c24a0
John Nolette <john@neetgroup.net>
2018-02-10 22:55:00 -0500

    Added basic styling, implemented react bulma components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import { Nav, NavItem } from 'reactbulma'
+import { Nav, NavLeft, NavRight, NavItem } from 'reactbulma'
 
 export default class Footer extends Component {
   render() {
     return(
       <footer>
-        <Nav>f</Nav>
+        <Nav>
+          <Nav.Left>
+            <Nav.Item>
+              <h1>Fork Me</h1>
+            </Nav.Item>
+          </Nav.Left>
+          <Nav.Right>
+            <Nav.Item>
+              <h1>Fork Me</h1>
+            </Nav.Item>
+          </Nav.Right>
+        </Nav>
       </footer>
     )
   }
